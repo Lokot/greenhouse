5b3b79e544d93ee51d63977b1d3f71e07c4b429a
Lokot <loktionovag@gmail.com>
2017-07-09 15:37:01 +0300

    update cron fields in config view

--- greenhouse-web/src/main/java/ru/skysoftlab/greenhouse/ui/SystemConfig.java
+++ greenhouse-web/src/main/java/ru/skysoftlab/greenhouse/ui/SystemConfig.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ru.skysoftlab.crongen.CronGenExt;
+import ru.skysoftlab.crongen.ParseCronGenComponent;
 import ru.skysoftlab.greenhouse.common.CustomAutoGableConverter;
 import ru.skysoftlab.greenhouse.dto.SystemConfigDto;
 import ru.skysoftlab.greenhouse.impl.DataBaseProvider;
@@ -57,6 +58,7 @@ public class SystemConfig extends BaseMenuView implements Button.ClickListener, 
 	private static final long serialVersionUID = 2039928987238266962L;
 
 	private Logger LOG = LoggerFactory.getLogger(SystemConfig.class);
+	private final Locale ruLocale = new Locale("ru");
 
 	@Inject
 	private DataBaseProvider dataBaseProvider;
@@ -70,10 +72,9 @@ public class SystemConfig extends BaseMenuView implements Button.ClickListener, 
 	// @RequestScoped
 	private SystemConfigDto dto;
 
-	private TextField scanInterval = new TextField("Интервал сканирования (cron):");
-	private Label scanIntervalLabel = new Label();
-	private TextField dataInterval = new TextField("Интервал получения данных (cron):");
-	private Label dataIntervalLabel = new Label();
+	private ParseCronGenComponent scanInterval = new ParseCronGenComponent("Интервал сканирования (cron):", ruLocale);
+	private ParseCronGenComponent dataInterval = new ParseCronGenComponent("Интервал получения данных (cron):",
+			ruLocale);
 	private OptionGroup auto = new OptionGroup("Управление коньком:");
 	private ComboBox serialPort = new ComboBox("Порт для связи с Arduino:");
 	private TextField tempMax = new TextField("Максимальная температура (°C):");
@@ -87,30 +88,6 @@ public class SystemConfig extends BaseMenuView implements Button.ClickListener, 
 
 	@Override
 	protected void configureComponents() {
-		CronGenExt ext = new CronGenExt();
-		ext.extend(scanInterval);
-		scanInterval.addTextChangeListener(new TextChangeListener() {
-
-			private static final long serialVersionUID = 6560987133628416549L;
-
-			@Override
-			public void textChange(TextChangeEvent event) {
-				scanIntervalLabel.setCaption(cronToString(event.getText()));
-			}
-		});
-		scanIntervalLabel.setCaption(cronToString(dto.getScanInterval()));
-		CronGenExt ext1 = new CronGenExt();
-		ext1.extend(dataInterval);
-		dataInterval.addTextChangeListener(new TextChangeListener() {
-
-			private static final long serialVersionUID = 6560987133628416549L;
-
-			@Override
-			public void textChange(TextChangeEvent event) {
-				dataIntervalLabel.setCaption(cronToString(event.getText()));
-			}
-		});
-		dataIntervalLabel.setCaption(cronToString(dto.getDataInterval()));
 		// illumMin.setMin(0);
 		// illumMin.setMax(1000);
 		// illumMin.setWidth("300px");
@@ -126,8 +103,8 @@ public class SystemConfig extends BaseMenuView implements Button.ClickListener, 
 	@Override
 	protected void buildLayout() {
 		Label title = new Label("Системные настройки");
-		FormLayout form = new FormLayout(scanInterval, scanIntervalLabel, dataInterval, dataIntervalLabel, auto,
-				serialPort, tempMax, temp2, temp1, tempMin, humMax, save);
+		FormLayout form = new FormLayout(scanInterval, dataInterval, auto, serialPort, tempMax, temp2, temp1, tempMin,
+				humMax, save);
 		formFieldBindings = BeanFieldGroup.bindFieldsBuffered(dto, this);
 		VerticalLayout left = new VerticalLayout(title, form);
 		left.setExpandRatio(form, 1);
@@ -163,15 +140,6 @@ public class SystemConfig extends BaseMenuView implements Button.ClickListener, 
 	@Override
 	public void valueChange(ValueChangeEvent event) {
 
-	}
-
-	private String cronToString(String cronExpr) {
-		try {
-			return CronExpressionDescriptor.getDescription(cronExpr, new Locale("ru"));
-		} catch (ParseException e1) {
-			e1.printStackTrace();
-			return "";
-		}
 	}
 
 }
