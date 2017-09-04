32e028866320f8b4159cffa66bc686eb50d9a5e3
U-Lokot-PC\Lokot <loktionov@lanit.ru>
2017-09-04 17:12:16 +0300

    add role annatation

--- greenhouse-web/src/main/java/ru/skysoftlab/greenhouse/ui/UsersView.java
+++ greenhouse-web/src/main/java/ru/skysoftlab/greenhouse/ui/UsersView.java
@@ -1,16 +1,20 @@
 package ru.skysoftlab.greenhouse.ui;
 
+import ru.skysoftlab.skylibs.security.RolesList;
 import ru.skysoftlab.skylibs.web.annatations.MainMenuItem;
 import ru.skysoftlab.skylibs.web.annatations.MenuItemView;
 import ru.skysoftlab.skylibs.web.ui.views.UserView;
 import ru.skysoftlab.greenhouse.web.MainMenu;
 import ru.skysoftlab.greenhouse.web.MainMenu.ConfigMenu;
 
+import javax.annotation.security.RolesAllowed;
+
 import com.vaadin.cdi.CDIView;
 
 @CDIView(MainMenu.USERS)
 @MainMenuItem(name = "Настройки", order = MainMenu.CONFIG)
 @MenuItemView(name = "Пользователи", order = ConfigMenu.CONFIG_USERS)
+@RolesAllowed({ RolesList.ADMIN })
 public class UsersView extends UserView {
 
 	private static final long serialVersionUID = 2125858000767147430L;
